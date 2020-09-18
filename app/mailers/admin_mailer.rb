37af803a901748ad1e72b0c9c2812db7b6dffdf9
ZeddBox <jul33anc@gmail.com>
2020-09-18 17:51:28 +0200

    fix: admin_mailer.rb

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -10,4 +10,6 @@ class AdminMailer < ApplicationMailer
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+
+  end
 end
