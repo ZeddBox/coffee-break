4f2a14133c20ce3692b301e5e6329cba8d18779f
ZeddBox <jul33anc@gmail.com>
2020-09-24 00:33:33 +0200

    feat design

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'https://coffee-break-dev.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://coffeebreak.pro/' }
 
 
 
