823629b27631c43053c83c5864320f9882f518e4
ZeddBox <jul33anc@gmail.com>
2020-10-06 23:05:48 +0200

    mention legale : 1#

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/a-propos-de-moi', to: 'home#about'
+  get '/mentions-legales', to: 'home#mentions'
+
 
   devise_for :users, path: 'gestion', path_names: { sign_in: 'se-connecter', sign_out: 'deconnexion', password: 'secret', confirmation: 'verification', unlock: 'deblocage', registration: 'compte', sign_up: 'inscription' }
 
